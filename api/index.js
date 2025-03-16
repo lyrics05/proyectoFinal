7a3418e1bdd4a01e3abb8daaf4d7b10592ade783
lyrics05 <dca49@hotmail.com>
2025-03-16 00:10:24 -0600

    actualizado

--- api/index.js
+++ api/index.js
@@ -1,13 +1,12 @@
 require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const { PORT } = process.env;
 
-
+const PORT = process.env.PORT || 3000;
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: true }).then(() => {
   server.listen(PORT, () => {
-    console.log(`escuchando en el puert ${PORT}`); // eslint-disable-line no-console
+    console.log(`escuchando en el puert ${PUERTO || process.env.PORT}`); // eslint-disable-line no-console
   });
 });
 
--- api/src/db.js
+++ api/src/db.js
@@ -3,10 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-  port: process.env.DB_PORT,
+const sequelize = new Sequelize(process.env.DATABASE_URL,{
   logging: false,
   pool:{
     max:5,
